64dd04743ec0a769bf63e06e0ed34857dfa64ee0
pp <prashanthpotragalla@gmail.com>
2026-09-18 16:02:52 +0530

    Update image source URLs for eye icon in verify-signin.js

--- assets/scripts/verify-signin.js
+++ assets/scripts/verify-signin.js
@@ -720,7 +720,7 @@ $(document).ready(function () {
       $passwordInput.css('paddingRight', '36px');
 
       var $eyeIcon = $('<img>', {
-        src: 'https://opaleyes.com/adb2c/assets/images/eye-off.svg',
+        src: 'https://prashanth-devp.github.io/ckY2gGCY9SZg/assets/images/eye-off.svg',
         alt: 'Toggle visibility',
       }).css({
         position: 'absolute',
@@ -740,8 +740,8 @@ $(document).ready(function () {
         $eyeIcon.attr(
           'src',
           visible
-            ? 'https://opaleyes.com/adb2c/assets/images/eye.svg'
-            : 'https://opaleyes.com/adb2c/assets/images/eye-off.svg',
+            ? 'https://prashanth-devp.github.io/ckY2gGCY9SZg/assets/images/eye.svg'
+            : 'https://prashanth-devp.github.io/ckY2gGCY9SZg/assets/images/eye-off.svg',
         );
       });
 
